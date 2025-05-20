67f9f6b7caef068dac4c9c26a0a3c8b6c6c36302
TANU YADAV <105559613+tanuu2905@users.noreply.github.com>
2025-05-20 11:35:11 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://tamatar-tota-backend.onrender.com";
+  const url = "https://foodie-backend-azqw.onrender.com";
   const [token, setToken] = useState("")
 
   const [food_list, setFoodlist] = useState([])
